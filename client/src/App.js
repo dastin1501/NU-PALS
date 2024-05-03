0acf77d995b4849ed8ddc1392ae056f0c209919f
dastin1501 <dastincontento@gmail.com>
2024-05-04 01:45:06 +0800

    2nd Add

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+// App.js
+import React from 'react';
+import LoginForm from './LoginForm';
+import './App.css'; // Import CSS file
+import Logo from './images/NUlogo.png'
+
+function App() {
+  return (
+    <>
+    <div className="app-container">
+      <div className="left-section">
+      <img src={Logo}/>
+      </div>
+      <div className="right-section">
+        <LoginForm />
+        <div className="navigation">
+          <a href="/home.js">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
